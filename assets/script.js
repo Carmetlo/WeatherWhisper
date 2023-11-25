6bc2b4b2dba0ec30a9438e49a9515ee3942a22de
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-25 08:49:18 -0700

    Initial commit for the day.  API does not work

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
-const apiKey = 'Y6a6da27e7d121487b839ad8ea7dd1de0';
+const apiKey = '1517baaf9a9a7ffd971be9a80da4eedb';
 const geocodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q='
-const apiUrl = 'https://api.openweathermap.org/data/2.5/';
-const latitude = data{ 0}.lat;
-const longitude = data{ 0}.lon;
 
-document.addEventListener('DOMContentLoaded', function () {
+function getCoordinates(city) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                const latitude = data[0].lat;
+                const longitude = data[0].lon;
+                console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
+                getWeather(latitude, longitude);
+            } else {
+                console.error('No results found for this $(city)');
+            }
+        })
+        .catch(error => console.error('Error fetching coordinates: ', error));
+}
+function getWeather(latitude, longitude) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/';
 
-    function getCoordinates(city) {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                if (data.length > 0) {
-                    console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
-                    getWeather(latitude, longitude);
-                } else {
-                    console.error('No results found for this $(city)');
-                }
-            })
-            .catch(error => console.error('Error fetching coordinates: ', error));
-    }
-    function getWeather(latitude, longitude) {
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                updateCurrentWeather(data);
-                updateForecast(data);
-                addToSearchHistory(city);
-            })
-            .catch(error => console.error('Error fetching data: ', error));
-    })
+    fetch(`${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            updateCurrentWeather(data);
+            updateForecast(data);
+            addToSearchHistory(city);
+        })
+        .catch(error => console.error('Error fetching data: ', error));
+}
