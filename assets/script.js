4f9c441e5c0bfec01019fdda4e07698d17da49c9
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-25 02:05:45 -0700

    finished with js weather api code js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,25 @@
 const apiKey = 'Y6a6da27e7d121487b839ad8ea7dd1de0';
+const geocodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q='
 const apiUrl = 'https://api.openweathermap.org/data/2.5/';
+const latitude = data{ 0}.lat;
+const longitude = data{ 0}.lon;
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    function getWeather(city) {
-        fetch(`{apiUrl}?q=${city}&appid=${apiKey}`)
+    function getCoordinates(city) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length > 0) {
+                    console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
+                    getWeather(latitude, longitude);
+                } else {
+                    console.error('No results found for this $(city)');
+                }
+            })
+            .catch(error => console.error('Error fetching coordinates: ', error));
+    }
+    function getWeather(latitude, longitude) {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
