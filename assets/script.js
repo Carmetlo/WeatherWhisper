0a902ad0b91f3ca38fc1f21b069304beaa783517
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-25 01:54:52 -0700

    added api key

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'YOUR_API_KEY';
+const apiKey = 'Y6a6da27e7d121487b839ad8ea7dd1de0';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
 document.addEventListener('DOMContentLoaded', function () {
