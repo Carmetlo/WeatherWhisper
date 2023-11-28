aa0acf0f88bdab29a8dbedf86055317bab0841fa
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-27 18:16:43 -0700

    Got complete 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,104 @@
 var apiKey = '1517baaf9a9a7ffd971be9a80da4eedb';
 const geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid={API key}'
 
+
 function getCoordinates(city) {
-    let url = geocodeURL.replace('{city name}', city).replace('{API key}', apiKey);
-    console.log(url);
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                const latitude = data[0].lat;
-                const longitude = data[0].lon;
-                console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
-                getWeather(latitude, longitude);
-            } else {
-                console.error('No results found for this $(city)');
-            }
-        })
-        .catch(error => console.error('Error fetching coordinates: ', error));
+    return new Promise((resolve, reject) => {
+        if (!city) {
+            console.error('City is undefined or null.');
+            reject('No city provided');
+            return;
+        }
+
+        let url = geocodeURL.replace('{city name}', city).replace('{API key}', apiKey);
+        console.log(url);
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.length > 0) {
+                    const latitude = data[0].lat;
+                    const longitude = data[0].lon;
+                    console.log(`Coordinates for ${city}: Latitude ${latitude}, Longitude ${longitude}`);
+                    getWeather(latitude, longitude);
+                    resolve();
+                } else {
+                    console.error(`No results found for ${city}`);
+                    reject('No results');
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching coordinates: ', error);
+                reject(error);
+            });
+    });
 }
-function getWeather(latitude, longitude) {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+function getWeather(latitude, longitude, city) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
     fetch(`${apiUrl}?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            updateCurrentWeather(data);
-            // updateForecast(data);
-            // addToSearchHistory(city);
+            // updateCurrentWeather(data);
+            updateForecast(data, city);
+            addToSearchHistory(city);
         })
         .catch(error => console.error('Error fetching data: ', error));
 }
-function updateCurrentWeather (data) {
-    var card = $("<div>").addClass ("card");
-    var cardTitle = $("<h2>").addClass("card-title").text(data.name)
-    var tempEl = $("<h3>").addClass("card-text").text("Temperature: "+Math.round(data.main.temp)+String.fromCharCode(186))
-    var humidtyEl = $("<h3>").addClass("card-text").text("Humidity: "+data.main.humidity+String.fromCharCode(37))
-    var windEl = $("<h3>").addClass("card-text").text("Wind Speed: "+data.wind.speed+" MPH")
-    var icon = $("<img>").attr("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-    $("#current-weather").append(card.append(cardTitle.append(icon),tempEl,humidtyEl,windEl))
+
+function updateForecast(data, city) {
+    const forecastEL = document.getElementById('forecast');
+    forecastEL.innerHTML = '<h2>5-Day Forecast:</h2>';
+
+    for (let i = 0; i < data.list.length; i += 8) {
+        const date = new Date(data.list[i].dt * 1000);
+        const iconCode = data.list[i].weather[0].icon;
+        const temperature = data.list[i].main.temp;
+        const windSpeed = data.list[i].wind.speed;
+        const humidity = data.list[i].main.humidity;
+
+        forecastEL.innerHTML += `
+            <div class="forecast-item">
+                <p>Date: ${date.toDateString()}</p>
+                <img src="https://openweathermap.org/img/wn/${iconCode}@2x.png" alt="Weather Icon">
+                <p>temperature: ${temperature} &deg;F</p>
+                <p>wind speed: ${windSpeed} MPH</p>
+                <p>humidity: ${humidity} %</p>
+            </div>
+        `;
+    }
 }
-document.getElementById('search').addEventListener("submit", function (event){
+const searchHistoryEL = document.getElementById('search-history');
+
+function addToSearchHistory(city) {
+
+    searchHistoryEL.innerHTML += `<p>${city}</p>`;
+}
+
+searchHistoryEL.addEventListener('click', async function (event) {
+    if (event.target.tagName === 'P') {
+        const clickedCity = event.target.textContent;
+        await getCoordinates(clickedCity);
+    }
+});
+
+function updateCurrentWeather(data) {
+    var card = $("<div>").addClass("card");
+    var cardTitle = $("<h2>").addClass("card-title").text(data.name)
+    var tempEl = $("<h3>").addClass("card-text").text("Temperature: " + Math.round(data.main.temp) + String.fromCharCode(186))
+    var humidtyEl = $("<h3>").addClass("card-text").text("Humidity: " + data.main.humidity + String.fromCharCode(37))
+    var windEl = $("<h3>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH")
+    var icon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+    $("#current-weather").append(card.append(cardTitle.append(icon), tempEl, humidtyEl, windEl))
+}
+document.getElementById('search').addEventListener("submit", function (event) {
     event.preventDefault()
     var cityName = document.getElementById("city-input").value
-    getCoordinates (cityName)
+    getCoordinates(cityName)
 })
